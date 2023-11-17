9f6f9eee5265d3e562fbd6361e1bfe21457d7a51
gio-rodrigues0 <giovanna.rodrigues@sou.inteli.edu.br>
2023-11-17 15:45:11 -0300

    o que é a vida? a vida é uma maravilha

--- chat.py
+++ chat.py
@@ -1,31 +1,28 @@
 import re
 
-def atualizar_pagamento():
-    return "Entrando na atualização de pagamento"
+def atualizar_pagamento(_):
+    return "Entrando na atualização de pagamento!"
 
-def status_pedido():
-    return "Entrando no acompanhamento de pedido"
-
-def sair():
-    return False
+def status_pedido(_):
+    return "Entrando no acompanhamento de pedido!"
 
 intent_dict = {
     r"(?i)(?:status|rastrear|pedido|entrega)": "status_pedido",
     r"(?i)(?:atualizar|mudar|cartão|pagamento)": "atualizar_pagamento",
-    r"(?i)(?:sair)": "sair"
 }
 
 action_dict = {
     "status_pedido": status_pedido,
     "atualizar_pagamento": atualizar_pagamento,
-    "sair": sair
 }
 
 while True:
     command = input("Digite o que precisa:")
+    if command == "sair":
+        break
     for key, value in intent_dict.items():
         pattern = re.compile(key)
         groups = pattern.findall(command)
         if groups:
-            print(f"{action_dict[value](groups[0])}", end=" ")
+            print(f"{action_dict[value](groups[0])}")
     print()
